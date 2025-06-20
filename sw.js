364f276d43ee1b800ade2ffaa6b00b7455c4ee18
antoGueF <antonio_guerrero@outlook.com>
2025-06-20 18:26:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker para la app de lista de la compra
-const CACHE_VERSION = 'shopping-list-v1.0.17';
+const CACHE_VERSION = 'shopping-list-v1.0.21';
 const CACHE_NAME = `shopping-list-${CACHE_VERSION}`;
 
 // Archivos a cachear (ajusta según tus archivos)
