913fc08e5011ba30df1bccd1d0e99b5e97b0cba8
antoGueF <antonio_guerrero@outlook.com>
2025-06-26 18:32:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker para la app de lista de la compra
-const CACHE_VERSION = 'shopping-list-v1.10.20';
+const CACHE_VERSION = 'shopping-list-v1.01.21';
 const CACHE_NAME = `shopping-list-${CACHE_VERSION}`;
 
 self.addEventListener('message', event => {
